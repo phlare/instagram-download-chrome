17fa262cdc543e61ef023e0bcc3d603ca424b01a
Mark Nelson <phlarebot@gmail.com>
2016-09-01 12:57:14 -0600

    POC complete
    added appropriate behavior and style for buttons.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var IDown = {
 		"_42elc" : "profile"
 	},
 	init: function() {
-		var self = this;
 		// the main loop of this script, adding buttons to images	
 		var loader = setInterval(function() {
 			// wait until jQuery is loaded
@@ -17,16 +16,15 @@ var IDown = {
 			clearInterval(loader);
 
 			// begin the inner interval looking for new images to add a download link to
-			self.detectInterval = setInterval(function() {
-				self.detectPage();
-			}, 250);
+			IDown.detectInterval = setInterval(function() {
+				IDown.detectPage();
+			}, 500);
 
 		}, 250);
 	},
 
 	// figure out what page we're on so we know what to do.
 	detectPage: function() {
-		var self = this;
 		var page = 'unknown';
 
 		// these are subject to change, I imagine.  
@@ -43,11 +41,58 @@ var IDown = {
 
 	// find images on the page and add button links for download
 	findImages: function(page) {
+
 		if (typeof page === 'undefined') {
 			return false;
 		}
 
-		console.log('seeking images for page ' + page);
+		// console.log('seeking images for page ' + page);
+		switch (page) {
+			case 'profile':
+				$('a._8mlbc:not(.idowned):first').each(function(i) {
+					var self = this;
+					// mark as being tracked
+					$(self).addClass('idowned');
+					
+					IDown.drawButton(self, 'profile');
+					
+				});
+				break;
+			case 'feed':
+				$('article._8ab8k:not(.idowned):first').each(function(i) {
+					var self = this;
+					// mark it as being tracked
+					$(self).addClass('idowned');
+
+					IDown.drawButton(self);
+
+		        });
+				break;
+			case 'unknown':
+			case 'default':
+				return false;
+		}
+	},
+	drawButton: function(ele, addClass) {
+		// get link if there's an image
+		var img = $(ele).find('div._jjzlb > img');
+		if (img.length) {
+			var img_src = img.attr('src');
+			var button_class = "iDownBtn";
+			if (typeof addClass === "string") {
+				button_class += ' ' + addClass;
+			}
+			var button_html = '<a class="' + button_class + '" href="' + img_src + '" download>&#8681</a>';
+			$(button_html)
+				.on('click', function(event) {
+					event.stopPropagation();
+				})
+				.hide()
+				.fadeIn()
+				.insertAfter(ele.lastChild);
+		} else {
+			return false;
+		}
 	}
 };
 
