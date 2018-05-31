916fa3597896da66e6595e78e462e8f548191d58
Mark Nelson <phlarebot@gmail.com>
2018-05-31 12:49:31 -0600

    got rid of a bunch of unneeded fluff.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var IDown = {
 	detectInterval: null,
 	pageReference: {
-		"_jx516" : "feed",
-		"_42elc" : "profile"
+		"_1SP8R" : "feed",
+		"_42elc" : "profile",
+	},
+	findClass: {
+		image: "KL4Bh"
 	},
 	init: function() {
 		// the main loop of this script, adding buttons to images	
@@ -17,14 +20,14 @@ var IDown = {
 
 			// begin the inner interval looking for new images to add a download link to
 			IDown.detectInterval = setInterval(function() {
-				IDown.detectPage();
+				IDown.detectImage();
 			}, 125);
 
 		}, 250);
 	},
 
 	// figure out what page we're on so we know what to do.
-	detectPage: function() {
+	detectImage: function() {
 		var page = 'unknown';
 
 		// these are subject to change, I imagine.  
@@ -32,19 +35,11 @@ var IDown = {
 		if ($('video:not(.idowned):first').length) {
 			var ele = $('video:not(.idowned):first').first();
 			IDown.findVideoInElement(ele);
-		} else if ($('img._2di5p:not(.idowned)').length) {
-			var ele = $('img._2di5p:not(.idowned):first').first();
+		} else if ($('img.FFVAD:not(.idowned)').length) {
+			var ele = $('img.FFVAD:not(.idowned):first').first();
 			IDown.findImageInPopup(ele);
-		} else if ($('main > article._42elc').length) {
-			// user profile page
-			page = "profile";
-		} else if ($('main > section._jx516').length) {
-			// feed page
-			page = "feed";
-		} else if ($('main > article._3n7ri').length) {
-			page = "hashtag";
 		}
-		IDown.findImages(page);
+		// IDown.findImages(page);
 	},
 	// find image on popup or page and add button link
 	findImageInPopup: function(ele) {
@@ -56,7 +51,7 @@ var IDown = {
 		var img_src = $(ele).attr('src');
 		var button_class = "iDownBtn profile";
 		var insertPoint = $(ele).parent();
-		var button_html = '<a class="' + button_class + '" href="' + img_src + '" download>&#8681</a>';
+		var button_html = '<a class="' + button_class + '" href="' + img_src + '" download target="_blank">&#8681</a>';
 		$(button_html)
 			.on('click', function(event) {
 				event.stopPropagation();
@@ -76,7 +71,7 @@ var IDown = {
 		var insertPoint = $(ele).parent().parent();
 		if (videoUrl.length) {
 			//TODO: refactor this to combine with the drawButton function
-			var button_html = '<a class="iDownBtn video" href="' + videoUrl + '" download>&#x25ba;</a>';
+			var button_html = '<a class="iDownBtn video" href="' + videoUrl + '" download target="_blank">&#x25ba;</a>';
 			
 			$(button_html)
 				.on('click', function(event) {
@@ -85,63 +80,6 @@ var IDown = {
 				.hide()
 				.fadeIn()
 				.insertAfter(insertPoint);
-		}
-
-	},
-	// find images on the page and add button links for download
-	findImages: function(page) {
-
-		if (typeof page === 'undefined') {
-			return false;
-		}
-
-		// console.log('seeking images for page ' + page);
-		switch (page) {
-			case 'profile':
-			case 'hashtag':
-				$('a._8mlbc:not(.idowned):first').each(function(i) {
-					var self = this;
-					// mark as being tracked
-					$(self).addClass('idowned');
-					
-					IDown.drawButton(self, 'profile');
-					
-				});
-				break;
-			case 'feed':
-				$('article._8ab8k:not(.idowned):first').each(function(i) {
-					var self = this;
-					// mark it as being tracked
-					$(self).addClass('idowned');
-
-					IDown.drawButton(self);
-
-		        });
-				break;
-			case 'unknown':
-			case 'default':
-				return false;
-		}
-	},
-	drawButton: function(ele, addClass) {
-		// get link if there's an image
-		var img = $(ele).find('div._jjzlb > img');
-		if (img.length) {
-			var img_src = img.attr('src');
-			var button_class = "iDownBtn";
-			if (typeof addClass === "string") {
-				button_class += ' ' + addClass;
-			}
-			var button_html = '<a class="' + button_class + '" href="' + img_src + '" download>&#8681</a>';
-			$(button_html)
-				.on('click', function(event) {
-					event.stopPropagation();
-				})
-				.hide()
-				.fadeIn()
-				.insertAfter(ele.lastChild);
-		} else {
-			return false;
 		}
 	}
 };
