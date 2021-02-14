788e434f727ea784cd4f948917232a314b7e950a
Mark Nelson <phlare@github.com>
2021-02-14 14:25:02 -0700

    add onlyfans

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var IDown = {
 	},
 	init: function() {
 		// the main loop of this script, adding buttons to images	
+		// console.log('narp');
 		var loader = setInterval(function() {
 			// wait until jQuery is loaded
 			if (typeof $ === "undefined") {
@@ -17,17 +18,38 @@ var IDown = {
 
 			// once loaded, stop the outer interval
 			clearInterval(loader);
-
+			// console.log('here');
 			// begin the inner interval looking for new images to add a download link to
 			IDown.detectInterval = setInterval(function() {
-				IDown.detectImage();
+				if(location.hostname == "onlyfans.com") {
+					IDown.detectOnlyFans();
+				} else if (location.hostname == "www.linkedin.com") {
+					IDown.detectLinkedinLike();
+				} else {
+					IDown.detectInstagram();	
+				}
+				
 			}, 125);
 
 		}, 250);
 	},
-
+	detectLinkedinLike: function() {
+		if ($('.reactions-react-button button li-icon[type=like-icon]:not(.idowned)').length) {
+			var ele = $('.reactions-react-button button li-icon[type=like-icon]:not(.idowned)').first();
+			IDown.autoLikeLinkedin(ele);
+		}
+	},
+	detectOnlyFans: function() {
+		if ($('.b-post__media__item-inner img:not(.idowned)').length) {
+			var ele = $('.b-post__media__item-inner img:not(.idowned)').first();
+			IDown.findImageOnlyFans(ele);
+		} else if ($('video:not(.idowned):first').length) {
+			var ele = $('video:not(.idowned):first').first();
+			IDown.findVideoOnlyFans(ele);
+		}
+	},
 	// figure out what page we're on so we know what to do.
-	detectImage: function() {
+	detectInstagram: function() {
 		var page = 'unknown';
 
 		// these are subject to change, I imagine.  
@@ -40,6 +62,62 @@ var IDown = {
 			IDown.findImageInPopup(ele);
 		}
 		// IDown.findImages(page);
+	},
+	autoLikeLinkedin: function(ele) {
+		if (typeof ele === 'undefined') {
+			return false;
+		}
+		$(ele).addClass('idowned');
+		$(ele).click();
+	},
+	// find image on popup or page and add button link
+	findImageOnlyFans: function(ele) {
+		if (typeof ele === 'undefined') {
+			return false;
+		}
+		// add class so we don't try again
+		$(ele).addClass('idowned');
+		var img_src = $(ele).attr('src');
+		var img_srcset = $(ele).attr('srcset');
+		if (typeof img_srcset !== 'undefined'){
+			img_srcset = img_srcset.split(" ")[0];
+		}
+		if (typeof img_src == 'undefined' && typeof img_srcset == "string") {
+			img_src = img_srcset;
+		}
+		var button_class = "iDownBtn onlyfans";
+		var insertPoint = $(ele);
+		var button_html = '<a class="' + button_class + '" href="' + img_src + '" download target="_blank">&#8681</a>';
+		$(button_html)
+			.on('click', function(event) {
+				console.log('click');
+				event.stopPropagation();
+			})
+			.hide()
+			.fadeIn()
+			.insertBefore(insertPoint);
+	},
+	// find video on popup or page and add button link 
+	findVideoOnlyFans: function(ele) {
+		if (typeof ele === 'undefined') {
+			return false;
+		}
+		// add class so we don't try again for the same element
+		$(ele).addClass('idowned');
+		var videoUrl = $(ele).attr('src');
+		var insertPoint = $(ele).parent().parent();
+		if (videoUrl.length) {
+			//TODO: refactor this to combine with the drawButton function
+			var button_html = '<a class="iDownBtn onlyfans video" href="' + videoUrl + '" download target="_blank">&#x25ba;</a>';
+			
+			$(button_html)
+				.on('click', function(event) {
+					event.stopPropagation();
+				})
+				.hide()
+				.fadeIn()
+				.insertBefore(insertPoint);
+		}
 	},
 	// find image on popup or page and add button link
 	findImageInPopup: function(ele) {
